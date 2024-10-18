6d2b3a49ff9856f7f08204a93d0c2b7bab2b4e52
Dev Gana <yahaya128173908@gmail.com>
2024-10-18 19:47:10 +0100

    api route file

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TelegramController;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// Route::post('/telegram/webhook', [TelegramController::class, 'handleWebhook']);
+// Route::get('/telegram/set-webhook', [TelegramController::class, 'setWebhook']);
+// Route::post('/telegram/save', [TelegramController::class, 'handleStartCommand']);
