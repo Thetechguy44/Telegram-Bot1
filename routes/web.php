6181c3c005a8c9d7735cbcf5782b13f8d211c1c1
Dev Gana <yahaya128173908@gmail.com>
2024-10-18 19:50:32 +0100

    add routes links

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TelegramController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/', [TelegramController::class, 'showLoginPage'])->name('login');
+// Route::post('/webhook', [TelegramController::class, 'webhook']);
+// Route::get('/callback', [TelegramController::class, 'handleLogin'])->name('telegram.callback');
+
+Route::get('/', [TelegramController::class, 'index']);
+Route::post('/telegram/save-user', [TelegramController::class, 'saveUser']);
